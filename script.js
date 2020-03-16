2f5006588499e8f8f3af97c6d2b72ec731d2fc04
scadler <scadler@s.sfusd.edu>
2020-03-16 23:10:28 +0000

    shot works, changes color, and so does user border

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 //creating canvas & getting context
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
-
+const user = {
+    x : canvas.height/2,
+    y : canvas.height/2,
+    width : 15,
+    color : "White",
+    angleOld : 0,
+    angleNew : 0,
+    laser : "off",
+}
+const shot = {
+    x : "",
+    y : "",
+    angleOld : "",
+    color:"",
+    exists : false,
+}
 //draw functions 
 function drawRect(x, y, w, h, color){
     context.fillStyle = color;
     context.fillRect(x, y, w, h);
+}
+var colorI = 0
+function chooseColor(){
+       let color = (colorI % 300 < 100) ? "blue" : (colorI % 300 > 200) ? "yellow" : "red"
+       shot.color = color
 }
 var a = 0
 function drawUser(x,y,b){
@@ -13,30 +33,19 @@ function drawUser(x,y,b){
     var context = canvas.getContext("2d");
     context.save()
     context.translate(x,y)
-    context.rotate(user.angleOld);
-    if(user.laser === "on"){
-        context.moveTo(0, 0);
-        context.moveTo(15,-26);
-        context.lineTo(15,-1000);
-        context.lineWidth = 5;
-        context.strokeStyle = "#0000FF";
-        context.stroke();
-        if(Math.abs(ball.y - ball.radius)>7.5){
-            ball.color === "red"
-        }
-        console.log(user.x)
-    }
+    context.rotate(user.angleOld+ (2*Math.PI/3));
     context.beginPath();
-    context.fillStyle = "#FFCC00";
+    context.fillStyle = "#FFFFFF";
     let height = 30 * Math.cos(Math.PI / 6);
     context.moveTo(0, 0);
     context.lineTo(0+30, 0);
     context.lineTo(0+15, 0 - height);
     context.closePath();
     context.fill();
-    
+    context.lineWidth = 2;
+    context.strokeStyle = (shot.exists === true) ? "white" : shot.color;
+    context.stroke();
     context.restore();
-    
     user.x += 0.5 * Math.cos(user.angleOld - Math.PI/2);
     user.y += 0.5 * Math.sin(user.angleOld - Math.PI/2);
 }
@@ -48,28 +57,23 @@ function drawCircle(x, y, r, color){
     context.closePath();
     context.fill();
 }
+function drawShot(color){
+    shot.x += 1.5 * Math.cos(shot.angleOld - Math.PI/2);
+    shot.y += 1.5 * Math.sin(shot.angleOld - Math.PI/2);
+    context.fillStyle = color
+    context.beginPath();
+    context.arc(shot.x, shot.y, 7, 0, Math.PI*2, false);
+    context.closePath();
+    context.fill();
+    colorI++
+    if(shot.x > canvas.width ||  shot.x < 0 || shot.y > canvas.height ||  shot.y < 0){
+        shot.exists = false;
+    }
+}
 function drawText(text,x, y, color){
     context.fillStyle = color;
     context.font = "75px arial";
     context.fillText(text, x, y);
-}
-const user = {
-    x : canvas.height/2,
-    y : canvas.height/2,
-    width : 15,
-    color : "White",
-    angleOld : 0,
-    angleNew : 0,
-    laser : "off",
-}
-const ball = {
-    x : canvas.height/2,
-    y : canvas.height/2,
-    radius : 15,
-    speed : 7,
-    color : "White",
-    angleOld : 0,
-    angleNew : 0,
 }
 function update(){
    if(user.y < -10){
@@ -87,8 +91,10 @@ function update(){
 }
 function render(){
 drawRect(0, 0, canvas.width, canvas.height, "black");
-drawCircle(ball.x, ball.y, ball.radius, ball.color);
+// drawCircle(shot.x, shot.y, shot.radius, shot.color);
+drawShot(shot.color);
 drawUser(user.x, user.y,user.angleNew)
+chooseColor()
 }
 function game(){
     render();
@@ -107,8 +113,11 @@ function keyPressed(e){
     }
     else if(key == " ") {
     e.preventDefault();
-        if(user.angleNew === 0){ 
-            user.laser = "on";
+        if(shot.exists === false){
+            shot.angleOld = user.angleOld
+            shot.x = user.x
+            shot.y = user.y
+            shot.exists = true;
         }
   }
 }
